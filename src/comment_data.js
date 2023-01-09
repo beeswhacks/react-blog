5dff8a1db8c380ebb1565cc20118ee429888b0b0
Jack Preston <je.preston20@gmail.com>
2023-01-09 14:39:26 +0000

    Add comment function and sample data

---
+++ src/comment_data.js
@@ -0,0 +1,28 @@
+export const commentData = {
+    post: {
+        key: 123,
+        id: 123,
+        content: "My money doesn't jiggle jiggle, it folds",
+        user: 'Jack'
+    },
+    comments: [
+        {
+            key: 0,
+            id:0,
+            user: 'Louis',
+            content: 'It makes me wanna wiggle wiggle, for sure'
+        },
+        {
+            key: 1,
+            id: 1,
+            user: 'Jack',
+            content: 'Six foot two in a compact, no slack'
+        },
+        {
+            key: 2,
+            id: 2,
+            user: 'Louis',
+            content: 'But luckily the seats go back!'
+        }
+    ]
+};
